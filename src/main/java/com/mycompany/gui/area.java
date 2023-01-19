1fc2294f1c21a54f5a0fb8f65002f4e1b39d2976
hafuus <hafuus17@gmail.com>
2023-01-19 17:53:10 +0300

    initial commit

---
+++ src/main/java/com/mycompany/gui/area.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gui;
+
+/**
+ *
+ * @author HP
+ */
+class area {
+
+    static void setText(String toString) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
